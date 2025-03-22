d60e451976558e624a45c5875a102a9c8288af18
Twarita-lab <twarita.choudhury93@gmail.com>
2025-03-22 17:29:52 +0530

    loops and conditions code added

---
+++ IF_Else.js
@@ -0,0 +1,17 @@
+var i=3;
+
+if(i%2==0 && i%3==0){
+	console.log(i+" is an disisible by 2 & 3 number")
+}
+
+else if(i%2==0 || i%3==0 ){
+	console.log(i+" is an divisble by 2 & 3")
+{
+ if (i%3==0){
+	console.log(i+" is divisible by 3 only")
+}
+}
+}
+else{
+	console.log(i+" is not divisible by 2 or 3")
+}
---
+++ Loops.js
@@ -0,0 +1,18 @@
+var a=10
+
+while(a>=10){
+	console.log("priniting inside while loop")
+	a--;
+}
+
+
+//do while loop
+do{
+	console.log("priniting inside do block")
+}while(a>=10) console.log(a);
+
+
+//for loop
+for(var i=0; i<2; i++){
+	console.log(i+ " inside for loop")
+}
